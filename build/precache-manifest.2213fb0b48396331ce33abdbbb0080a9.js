efdd5faf8cc42e33530a4b0e6492361de5d94da4
TahaSener <tahakubilaysener@outlook.com>
2022-05-10 06:20:08 +0300

    Second Commit For Deploy

---
+++ build/precache-manifest.2213fb0b48396331ce33abdbbb0080a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "043d1816065cc663f9f4",
+    "url": "/tfjs-yolov5-example/static/css/main.a94688d0.chunk.css"
+  },
+  {
+    "revision": "043d1816065cc663f9f4",
+    "url": "/tfjs-yolov5-example/static/js/main.043d1816.chunk.js"
+  },
+  {
+    "revision": "8af9719caaa5636081f6",
+    "url": "/tfjs-yolov5-example/static/js/1.8af9719c.chunk.js"
+  },
+  {
+    "revision": "d686f760dfaf5cb8372f",
+    "url": "/tfjs-yolov5-example/static/js/runtime~main.d686f760.js"
+  },
+  {
+    "revision": "5b563b8af45e06671625426dbdbd6a46",
+    "url": "/tfjs-yolov5-example/index.html"
+  }
+];
