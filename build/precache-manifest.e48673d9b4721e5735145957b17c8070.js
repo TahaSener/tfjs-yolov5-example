9363acaa845d67f8cff8e5f3b6aa7f70c76a297c
TahaSener <tahakubilaysener@outlook.com>
2022-05-10 06:53:01 +0300

    bug fix

---
+++ build/precache-manifest.e48673d9b4721e5735145957b17c8070.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8e1dc2d468bfe720df6",
+    "url": "/tfjs-yolov5-example/static/css/main.a94688d0.chunk.css"
+  },
+  {
+    "revision": "b8e1dc2d468bfe720df6",
+    "url": "/tfjs-yolov5-example/static/js/main.b8e1dc2d.chunk.js"
+  },
+  {
+    "revision": "8af9719caaa5636081f6",
+    "url": "/tfjs-yolov5-example/static/js/1.8af9719c.chunk.js"
+  },
+  {
+    "revision": "d686f760dfaf5cb8372f",
+    "url": "/tfjs-yolov5-example/static/js/runtime~main.d686f760.js"
+  },
+  {
+    "revision": "4496d55b09ac7688bfb7276edcabfe10",
+    "url": "/tfjs-yolov5-example/index.html"
+  }
+];
