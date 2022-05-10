9162ccd9f0f2f1c922bcf1831a693630690624a5
TahaSener <tahakubilaysener@outlook.com>
2022-05-10 06:35:21 +0300

    Updates

---
+++ precache-manifest.54c4d772673767e8a9dfe1c5927b3a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "043d1816065cc663f9f4",
+    "url": "/tfjs-yolov5-example/static/css/main.a94688d0.chunk.css"
+  },
+  {
+    "revision": "043d1816065cc663f9f4",
+    "url": "/tfjs-yolov5-example/static/js/main.043d1816.chunk.js"
+  },
+  {
+    "revision": "37a5f069e075a4661e4b",
+    "url": "/tfjs-yolov5-example/static/js/1.37a5f069.chunk.js"
+  },
+  {
+    "revision": "d686f760dfaf5cb8372f",
+    "url": "/tfjs-yolov5-example/static/js/runtime~main.d686f760.js"
+  },
+  {
+    "revision": "36cb8396e4831fc7037a6f27da8265ae",
+    "url": "/tfjs-yolov5-example/index.html"
+  }
+];
